996264430e0323cbfd3fece5cb726791ebda8643
Tmonster <tom@ebergen.com>
2025-11-03 17:09:24 +0100

    modified generate large table

--- scripts/generate_large_iceberg_table.py
+++ scripts/generate_large_iceberg_table.py
@@ -11,39 +11,75 @@ import os
 
 
 CONNECTION_KEY = 'spark-rest'
-SPARK_RUNTIME_PATH = 'scripts/data_generators/iceberg-spark-runtime-3.5_2.12-1.4.1.jar'
+SPARK_RUNTIME_PATH = 'scripts/data_generators/iceberg-spark-runtime-3.5_2.12-1.9.0.jar'
 os.environ["PYSPARK_SUBMIT_ARGS"] = (
-    "--packages org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.4.1,org.apache.iceberg:iceberg-aws-bundle:1.9.0 pyspark-shell"
+    "--packages org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.9.0,org.apache.iceberg:iceberg-aws-bundle:1.9.0 pyspark-shell"
 )
-os.environ["AWS_REGION"] = "us-east-1"
-os.environ["AWS_ACCESS_KEY_ID"] = "admin"
-os.environ["AWS_SECRET_ACCESS_KEY"] = "password"
+# os.environ["AWS_REGION"] = "us-east-1"
+# os.environ["AWS_ACCESS_KEY_ID"] = "admin"
+# os.environ["AWS_SECRET_ACCESS_KEY"] = "password"
+# spark = (
+#     SparkSession.builder.appName("DuckDB REST Integration test")
+#     .config(
+#         "spark.sql.extensions",
+#         "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+#     )
+#     .config("spark.sql.catalog.demo", "org.apache.iceberg.spark.SparkCatalog")
+#     .config("spark.sql.catalog.demo.type", "rest")
+#     .config("spark.sql.catalog.demo.uri", "http://127.0.0.1:8181")
+#     .config("spark.sql.catalog.demo.warehouse", "s3://warehouse/wh/")
+#     .config("spark.sql.catalog.demo.s3.endpoint", "http://127.0.0.1:9000")
+#     .config("spark.sql.catalog.demo.s3.path-style-access", "true")
+#     .config('spark.driver.memory', '10g')
+#     .config("spark.sql.catalogImplementation", "in-memory")
+#     .config("spark.sql.catalog.demo.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
+#     .config('spark.jars', SPARK_RUNTIME_PATH)
+#     .getOrCreate()
+# )
+
 spark = (
     SparkSession.builder.appName("DuckDB REST Integration test")
     .config(
         "spark.sql.extensions",
         "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
     )
-    .config("spark.sql.catalog.demo", "org.apache.iceberg.spark.SparkCatalog")
-    .config("spark.sql.catalog.demo.type", "rest")
-    .config("spark.sql.catalog.demo.uri", "http://127.0.0.1:8181")
-    .config("spark.sql.catalog.demo.warehouse", "s3://warehouse/wh/")
-    .config("spark.sql.catalog.demo.s3.endpoint", "http://127.0.0.1:9000")
-    .config("spark.sql.catalog.demo.s3.path-style-access", "true")
+    .config('spark.sql.defaultCatalog', 'spark_catalog')
+    .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkCatalog")
+    .config("spark.sql.catalog.spark_catalog.type", "rest")
+    .config("spark.sql.catalog.spark_catalog.uri", "https://s3tables.us-east-2.amazonaws.com/iceberg")
+    .config("spark.sql.catalog.spark_catalog.warehouse", "arn:aws:s3tables:us-east-2:840140254803:bucket/new-iceberg-bucket")
+    # .config("spark.sql.catalog.spark_catalog.metrics-reporter-impl", "null")
+    .config("spark.sql.catalog.spark_catalog.s3.path-style-access", "true")
+    .config('spark.sql.catalog.spark_catalog.rest.auth.type','sigv4')
+    .config('spark.sql.catalog.spark_catalog.rest.signing-name','s3tables')
+    .config('spark.sql.catalog.spark_catalog.rest.signing-region', 'us-east-2')
+    .config("spark.sql.catalog.spark_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
     .config('spark.driver.memory', '10g')
     .config("spark.sql.catalogImplementation", "in-memory")
-    .config("spark.sql.catalog.demo.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
     .config('spark.jars', SPARK_RUNTIME_PATH)
     .getOrCreate()
 )
 
-table_identifier = "default.large_partitioned_table"
-spark.sql(f"""
-CREATE OR REPLACE TABLE {table_identifier} (id int, name string, joined timestamp)
-USING iceberg
-PARTITIONED BY (month(joined))
-TBLPROPERTIES ('format-version'=2, 'write.update.mode'='merge-on-read', 'write.delete.mode'='merge-on-read')
+table_identifier = "default.large_partitioned_table_for_vacuum"
+
+try:
+    spark.sql(f"""
+delete from {table_identifier} where 1=1;
 """)
+# if table not found, just continue
+except:
+    spark.sql(f"""
+        CREATE TABLE {table_identifier} (id int, name string, joined timestamp)
+        USING iceberg
+        PARTITIONED BY (month(joined))
+        TBLPROPERTIES (
+        'format-version'=2, 
+        'write.update.mode'='merge-on-read', 
+        'write.delete.mode'='merge-on-read',
+        'write.metadata.metrics.default' = 'none')
+    """)
+
+
 
 start_time = datetime(2000, 1, 1)
 end_time = datetime(2024, 12, 31)
@@ -57,7 +93,7 @@ schema = StructType([
     StructField("joined", TimestampType(), False)
 ])
 
-for snapshot_num in range(1, 2):
+for snapshot_num in range(1, 5):
     # Random interval between 10 and 10 days
     delta_days = random.randint(1, 4)
     current_time += timedelta(days=delta_days)
