605b70ab3a124d6a82fc51fe45023404b201fce1
Tmonster <tom@ebergen.com>
2026-07-16 17:12:22 +0200

    remove aws dependency entirely

--- src/include/catalog/rest/storage/aws.hpp
+++ src/include/catalog/rest/storage/aws.hpp
@@ -17,21 +17,17 @@ public:
 	unique_ptr<HTTPResponse> Request(RequestType request_type, ClientContext &context, HTTPHeaders &headers,
 	                                 const string &data);
 
-// 	unique_ptr<HTTPResponse> ExecuteRequestLegacy(ClientContext &context, Aws::Http::HttpMethod method,
-// 	                                              HTTPHeaders &headers, const string &body = "");
-// 	unique_ptr<HTTPResponse> ExecuteRequest(ClientContext &context, Aws::Http::HttpMethod method,
-// 	                                        unique_ptr<HTTPClient> &client, HTTPHeaders &headers,
-// 	                                        const string &body = "");
-	unique_ptr<HTTPResponse> ExecuteRequest(ClientContext &context, RequestType request_type,
-												  unique_ptr<HTTPClient> &client, HTTPHeaders &headers,
-												  const string &body = "");
-// 	std::shared_ptr<Aws::Http::HttpRequest> CreateSignedRequest(Aws::Http::HttpMethod method, const Aws::Http::URI &uri,
-// 	                                                            HTTPHeaders &headers, const string &body = "");
-// 	Aws::Http::URI BuildURI();
-// 	Aws::Client::ClientConfiguration BuildClientConfig();
-//
+	unique_ptr<HTTPResponse> ExecuteRequest(ClientContext &context, RequestType request_type, HTTPHeaders &headers,
+	                                        const string &body = "");
+
+private:
+	string GetURLEncodedPath() const;
+	string GetQueryString() const;
+
 public:
 	AttachedDatabase &attached_db;
+	//! The scheme to use for this request (http or https), defaults to https
+	string scheme = "https";
 	string authority;
 	vector<string> path_segments;
 	vector<std::pair<string, string>> query_string_parameters;
